struktured-labs/obs-studio

commit 1682d77df34a5c98ab24f41a273b29dd52e4f59e

obs-outputs: Fix rtmp authentication
Reinstate flag checks in RTMP_Close that were erroneously removed.

Clear out the Link state before we establish a new connection. There is
too much state carried around during authentication that has no good
place to clear it in librtmp, which assumes a clean structure when the
connection is initially established.

plugins/obs-outputs/librtmp/rtmp.c
@@ -4221,14 +4221,17 @@ RTMP_Close(RTMP *r)
     }
 
 #if defined(CRYPTO) || defined(USE_ONLY_MD5)
-    for (int idx = 0; idx < r->Link.nStreams; idx++)
+    if (!(r->Link.protocol & RTMP_FEATURE_WRITE) || (r->Link.pFlags & RTMP_PUB_CLEAN))
     {
-        free(r->Link.streams[idx].playpath.av_val);
-        r->Link.streams[idx].playpath.av_val = NULL;
-    }
+        for (int idx = 0; idx < r->Link.nStreams; idx++)
+        {
+            free(r->Link.streams[idx].playpath.av_val);
+            r->Link.streams[idx].playpath.av_val = NULL;
+        }
 
-    r->Link.curStreamIdx = 0;
-    r->Link.nStreams = 0;
+        r->Link.curStreamIdx = 0;
+        r->Link.nStreams = 0;
+    }
 
     if ((r->Link.protocol & RTMP_FEATURE_WRITE) &&
             (r->Link.pFlags & RTMP_PUB_CLEAN) &&

plugins/obs-outputs/rtmp-stream.c
@@ -394,6 +394,7 @@ static int try_connect(struct rtmp_stream *stream)
 
 	info("Connecting to RTMP URL %s...", stream->path.array);
 
+	memset(&stream->rtmp.Link, 0, sizeof(stream->rtmp.Link));
 	if (!RTMP_SetupURL(&stream->rtmp, stream->path.array))
 		return OBS_OUTPUT_BAD_PATH;
 

commit 764900c30453fa3c7309a1e3d351a9ada423f303

librtmp: Fix memory leak
A leak was introduced by commit 1682d77 (July 11 2015).
This is a fix.

plugins/obs-outputs/librtmp/rtmp.c
@@ -4288,14 +4288,14 @@ RTMP_Close(RTMP *r)
     r->m_customSendParam = NULL;
 
 #if defined(CRYPTO) || defined(USE_ONLY_MD5)
+    for (int idx = 0; idx < r->Link.nStreams; idx++)
+    {
+        free(r->Link.streams[idx].playpath.av_val);
+        r->Link.streams[idx].playpath.av_val = NULL;
+    }
+
     if (!(r->Link.protocol & RTMP_FEATURE_WRITE) || (r->Link.pFlags & RTMP_PUB_CLEAN))
     {
-        for (int idx = 0; idx < r->Link.nStreams; idx++)
-        {
-            free(r->Link.streams[idx].playpath.av_val);
-            r->Link.streams[idx].playpath.av_val = NULL;
-        }
-
         r->Link.curStreamIdx = 0;
         r->Link.nStreams = 0;
     }